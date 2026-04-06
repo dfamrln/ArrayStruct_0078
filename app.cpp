165738568c45eabfe6e23e738df923e3c7257691
dfamrln <mhdffa1@gmail.com>
2026-04-06 10:43:06 +0700

    mengisi data melalui perulangan

--- app.cpp
+++ app.cpp
@@ -32,4 +32,11 @@ int main()
     cout << "data ketiga = " << datamu[2] << endl;
     cout << "data keempat = " << datamu[3] << endl;
     cout << "data kelima = " << datamu[4] << endl;
+
+    // mengisi data melalui perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
 }
