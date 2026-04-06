2d6280ed11e7913b4e9116d757a3ed1cb2601600
dfamrln <mhdffa1@gmail.com>
2026-04-06 10:50:41 +0700

    menambahkan main dan isinya

--- struct.cpp
+++ struct.cpp
@@ -9,3 +9,14 @@ struct mahasiswa
     int umur;
 };
 
+int  main()
+{
+    // dekalarasi variabel struct
+    mahasiswa mhs;
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+}
