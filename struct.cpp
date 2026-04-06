a405822155308f5b869ac1d5daaca074cd9326eb
dfamrln <mhdffa1@gmail.com>
2026-04-06 10:53:15 +0700

    melengkapi isi main

--- struct.cpp
+++ struct.cpp
@@ -19,4 +19,10 @@ int  main()
     cout << "Isikan data alamat : ";
     cin >> mhs.alamat;
     cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    cout << "Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Usia : " << mhs.umur << endl;
 }
